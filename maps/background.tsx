7e55c6473cb9e7e9894307f3ffef2cdde9b72283
David Athay <ko2fan@gmail.com>
2023-05-29 19:37:40 +0200

    Add 2 characters and a plain background map.

---
+++ maps/background.tsx
@@ -0,0 +1,43 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="background" tilewidth="89" tileheight="89" tilecount="13" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="89" height="89" source="../sprites/grass_tiles_0000_Layer-55.png"/>
+ </tile>
+ <tile id="1">
+  <image width="89" height="89" source="../sprites/grass_tiles_0001_Layer-54.png"/>
+ </tile>
+ <tile id="2">
+  <image width="89" height="89" source="../sprites/grass_tiles_0002_Layer-53.png"/>
+ </tile>
+ <tile id="3">
+  <image width="89" height="89" source="../sprites/grass_tiles_0003_Layer-52.png"/>
+ </tile>
+ <tile id="4">
+  <image width="89" height="89" source="../sprites/grass_tiles_0004_Layer-51.png"/>
+ </tile>
+ <tile id="5">
+  <image width="89" height="89" source="../sprites/grass_tiles_0005_Layer-50.png"/>
+ </tile>
+ <tile id="6">
+  <image width="89" height="89" source="../sprites/grass_tiles_0006_Layer-49.png"/>
+ </tile>
+ <tile id="7">
+  <image width="89" height="89" source="../sprites/grass_tiles_0007_Layer-48.png"/>
+ </tile>
+ <tile id="8">
+  <image width="89" height="89" source="../sprites/grass_tiles_0008_Layer-47.png"/>
+ </tile>
+ <tile id="9">
+  <image width="89" height="89" source="../sprites/grass_tiles_0009_Layer-46.png"/>
+ </tile>
+ <tile id="10">
+  <image width="89" height="89" source="../sprites/grass_tiles_0010_Layer-45.png"/>
+ </tile>
+ <tile id="11">
+  <image width="89" height="89" source="../sprites/grass_tiles_0011_Layer-44.png"/>
+ </tile>
+ <tile id="12">
+  <image width="89" height="89" source="../sprites/grass_tiles_0012_Layer-43.png"/>
+ </tile>
+</tileset>
